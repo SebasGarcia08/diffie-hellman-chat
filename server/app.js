847454ecede636f7684703bf65371ee1b30dec06
sebastiangarciaacosta <sebastian.garcia@wearemo.com>
2021-12-07 11:36:19 -0500

    Added client

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('socket.io server');
-})
+});
 
+io.on('connection', (socket) => {
+  console.log('New socket connected');
+});
+
+
+http.listen(5000, ()=> {
+  console.log('NANDITO ES UN CANGURO')
+});
+
+module.exports = app;
