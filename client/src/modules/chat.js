cf46f5e05dba7e703208bbf2108dc493c4ffbf56
sebastiangarciaacosta <sebastian.garcia@wearemo.com>
2021-12-09 15:03:12 -0500

    Secret keys are iqual: FUCK YEAH!

--- client/src/modules/chat.js
+++ client/src/modules/chat.js
@@ -8,7 +8,8 @@ const {
   scryptSync,
   createDecipheriv,
   createCipher,
-  createHash
+  createHash,
+  DiffieHellman,
 } = require("crypto");
 
 const chatModule = {
@@ -20,7 +21,9 @@ const chatModule = {
     diffieHellmanObject: {},
     secret_key: null,
     public_key: null,
-    exchange: false
+    exchange: false,
+    private_key: null,
+    prime: null
   },
   // emit actions to nodejs from our app
   actions: {
@@ -31,38 +34,31 @@ const chatModule = {
       rootState.io.emit("login", username);
     },
     socket_diffieHellman: ({ rootState, commit }, { dstUser, srcUser }) => {
-      // Sender
-      const diffieHellmanObject = createDiffieHellman(256);
-      const juan_public_key = diffieHellmanObject
-        .generateKeys("hex")
-        .toString("hex"); // Juan This key should be transferred to the other party
-      let prime = diffieHellmanObject.getPrime("hex");
-      let generator = diffieHellmanObject.getGenerator("hex");
+      console.log("Calculating primes and generator...");
+      let dhObj = createDiffieHellman(256);
+      // Prime
+      let p = parseInt(dhObj.getPrime('hex'), 16);
+      console.log("Prime: " + p);
 
-      // console.log("Juan prime: " + prime);
-      // console.log("Juan prime type: " + typeof prime);
-      // console.log("Juan generator: " + generator);
-      // console.log("Juan generator type: " + typeof generator);
-      // console.log("Juan public_key: " + juan_public_key);
-      // console.log("Juan public_key type: " + typeof juan_public_key);
+      // Generator
+      let g = parseInt(dhObj.getGenerator('hex'), 16);
+      console.log("Generator: " + g);
 
-      // console.log("Juan public_key 1: ");
-      // console.log(typeof juan_public_key);
-      // console.log(juan_public_key);
-
-      let data = {
-        diffieHellmanObject: diffieHellmanObject,
-        dstUser: dstUser,
-        public_key: juan_public_key
-      };
-      commit("SET_DIFFIEHELLMAN_AND_PUBLIC_KEY", data);
+      // private key of Alice
+      let a = Math.floor(Math.random() * 9) + 1;
+      
+      // public key of Alice
+      let K_a = Math.pow(g, a) % p;
+      commit("SET_PRIVATE_AND_PUBLIC_KEY_AND_PRIME", {
+        private_key: a,
+        public_key: K_a,
+        prime: p
+      })
 
       let data_to_send = {
-        public_key: juan_public_key,
-        prime: prime,
-        generator: generator,
-        dstUser: dstUser,
-        srcUser: srcUser
+        public_key: K_a,
+        prime: p,
+        generator: g
       };
       rootState.io.emit("exchange_sender", data_to_send);
     },
@@ -75,125 +71,40 @@ const chatModule = {
     SET_USERNAME(state, username) {
       state.username = username;
     },
-    SET_DIFFIEHELLMAN_AND_PUBLIC_KEY(
+    SET_PRIVATE_AND_PUBLIC_KEY_AND_PRIME(
       state,
-      { dstUser, diffieHellmanObject, public_key }
+      { private_key, public_key, prime }
     ) {
-      state.diffieHellmanObject[dstUser] = diffieHellmanObject;
+      state.private_key = private_key;
       state.public_key = public_key;
+      state.prime = prime;
     },
-    SOCKET_GENERATE_SECRET_KEY_RECEIVER(state, data) {
-      // console.log("Sara prime: " + data["prime"]);
-      // console.log("Sara prime type: " + typeof data["prime"]);
-      // console.log("Sara generator: " + data["generator"]);
-      // console.log("Sara generator type: " + typeof data["generator"]);
-      // console.log("Sara public_key received: " + data["public_key"]);
-      // console.log(
-      //   "Sara public_key received type: " + typeof data["public_key"]
-      // );
-
-      // Receiver
-      const diffieHellmanObject = createDiffieHellman(
-        data["prime"],
-        "hex",
-        data["generator"],
-        "hex"
-      );
-
-      const sara_public_key = diffieHellmanObject
-        .generateKeys("hex")
-        .toString("hex"); // This key should be transferred to the other party
-      // console.log("public_key_receiver: " + public_key);
-      // console.log("public_key_data: ");
-      // console.log(data["public_key"].toString("hex"));
-
-      // console.log("Sara public key: " + sara_public_key);
-      // console.log("Sara public key type: " + typeof sara_public_key);
-
-      console.log(
-        "Sara diffObj prime: " + diffieHellmanObject.getPrime().toString("hex")
-      );
-      console.log(
-        "Sara diffObj generator: " +
-          diffieHellmanObject.getGenerator().toString("hex")
-      );
-
-      const sara_secret_key = diffieHellmanObject
-        .computeSecret(data["public_key"], "hex", "hex")
-        .toString("hex"); // Bobs does this
-
-      console.log(
-        "Sara createDiffieHellman Prime",
-        diffieHellmanObject.getPrime().toString("hex")
-      );
-      console.log(
-        "Sara createDiffieHellman Generator",
-        diffieHellmanObject.getGenerator().toString("hex")
-      );
-      console.log(
-        "Sara createDiffieHellman Public Key",
-        diffieHellmanObject.getPublicKey().toString("hex")
-      );
-      console.log(
-        "Sara createDiffieHellman Private Key",
-        diffieHellmanObject.getPrivateKey().toString("hex")
-      );
-
-      console.log("Sara secret_key: " + sara_secret_key);
-      console.log("Sara secret_key type: " + typeof sara_secret_key);
-      // console.log("Sara secret_key: ");
-      // console.log(sara_secret_key);
-
-      state.diffieHellmanObject[data["srcUser"]] = diffieHellmanObject;
-      state.secret_key = sara_secret_key;
-      state.public_key = sara_public_key;
-
-      // Set exchange to true in order to send public key to the sender
+    SOCKET_GENERATE_SECRET_KEY_RECEIVER(state, {
+      public_key,
+      prime,
+      generator
+    }) {
+      let K_a = public_key
+      // Bob's private key 
+      let b = Math.floor(Math.random() * 9) + 1;
+      // Bob's public key
+      let K_b = Math.pow(generator, b) % prime;
+      // Bob's secret key
+      let B = Math.pow(K_a, b) % prime;
+      console.log("Bob's secret key: " + B); 
+      state.secret_key = B;
+      state.private_key = b;
+      state.public_key = K_b;
+      state.prime = prime;
       state.exchange = true;
-
-      //let data_to_send = { public_key: data["public_key"].toString("hex") };
-
-      // TODO: Send to Socket
     },
-    SOCKET_GENERATE_SECRET_KEY_SENDER(state, { public_key, receiver }) {
-      // console.log("Juan public_key received: " + public_key);
-      // console.log("Juan public_key received type: " + typeof public_key);
-
-      const diffieHellmanObject = state.diffieHellmanObject[receiver];
-
-      console.log(
-        "Juan diffObj prime: " + diffieHellmanObject.getPrime().toString("hex")
-      );
-      console.log(
-        "Juan diffObj generator: " +
-          diffieHellmanObject.getGenerator().toString("hex")
-      );
-
-      const juan_secret_key = diffieHellmanObject
-        .computeSecret(public_key, "hex", "hex")
-        .toString("hex");
-
-      console.log(
-        "Juan createDiffieHellman Prime",
-        diffieHellmanObject.getPrime().toString("hex")
-      );
-      console.log(
-        "Juan createDiffieHellman Generator",
-        diffieHellmanObject.getGenerator().toString("hex")
-      );
-      console.log(
-        "Juan createDiffieHellman Public Key",
-        diffieHellmanObject.getPublicKey().toString("hex")
-      );
-      console.log(
-        "Juan createDiffieHellman Private Key",
-        diffieHellmanObject.getPrivateKey().toString("hex")
-      );
-
-      console.log("Juan secret_key: " + juan_secret_key);
-      console.log("Juan secret_key type: " + typeof juan_secret_key);
-
-      state.secret_key = juan_secret_key;
+    SOCKET_GENERATE_SECRET_KEY_SENDER(state, { public_key }) {
+      let K_b = public_key;
+      let a = state.private_key;
+      // Alice's secret key
+      let A = Math.pow(K_b, a) % state.prime;
+      state.secret_key = A;
+      console.log("Alice's secret key: " + state.secret_key)
     },
     SOCKET_NEW_MESSAGE(state, { message, srcUser, dstUser }) {
       console.log(state.conversations);
